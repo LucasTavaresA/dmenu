002d5fc816e9e3545b42ad1261be9168f1870263
LucasTavaresA <tavares.lassuncao@gmail.com>
2021-12-07 13:19:21 -0300

    Pequnas mudanças na aparencia

--- config.def.h
+++ config.def.h
@@ -11,14 +11,14 @@ static const char *prompt      = NULL;      /* -p  option; prompt to the left of
 static const char *colors[SchemeLast][2] = {
 	/*                         fg         bg       */
 	[SchemeNorm] = {          "#FFFFFF", "#000000" },
-	[SchemeSel] = {           "#FFFFFF", "#0000FF" },
-	[SchemeSelHighlight] = {  "#FFFF00", "#0000FF" },
+	[SchemeSel] = {           "#000000", "#FFFFFF" },
+	[SchemeSelHighlight] = {  "#FF0000", "#FFFFFF" },
 	[SchemeNormHighlight] = { "#FFFF00", "#000000" },
-	[SchemeOut] = {           "#000000", "#FFFFFF" },
+	[SchemeOut] = {           "#FFFFFF", "#000000" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines      = 35;
-static unsigned int columns    = 5;
+static unsigned int lines      = 20;
+static unsigned int columns    = 6;
 
 /*
  * Characters not considered part of a word while deleting words
