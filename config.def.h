b7cb55b392ebcce831933863f599c2d957400f43
LucasTavaresA <tavares.lassuncao@gmail.com>
2021-12-11 01:09:53 -0300

    Fontes e cores trocadas para mais visibilidade

--- config.def.h
+++ config.def.h
@@ -5,20 +5,20 @@ static int topbar = 0;                      /* -b  option; if 0, dmenu appears a
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=11"
+	"monospace:size=12"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*                         fg         bg       */
 	[SchemeNorm] = {          "#FFFFFF", "#000000" },
-	[SchemeSel] = {           "#000000", "#FFFFFF" },
-	[SchemeSelHighlight] = {  "#FF0000", "#FFFFFF" },
+	[SchemeSel] = {           "#FFFFFF", "#555555" },
+	[SchemeSelHighlight] = {  "#FF0000", "#555555" },
 	[SchemeNormHighlight] = { "#FFFF00", "#000000" },
 	[SchemeOut] = {           "#FFFFFF", "#000000" },
 };
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
 static unsigned int lines      = 20;
-static unsigned int columns    = 6;
+static unsigned int columns    = 1;
 
 /*
  * Characters not considered part of a word while deleting words
