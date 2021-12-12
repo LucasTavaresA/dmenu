168bed6b48c9ab148083e60e01a3b6e2f2a7dab7
LucasTavaresA <tavares.lassuncao@gmail.com>
2021-12-12 11:05:29 -0300

    Atualizada screenshot

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
-static const unsigned int alpha = 0xb3;
+static const unsigned int alpha = 0xe6;
 static int centered = 0;                    /* -c option; centers dmenu on screen */
 static int min_width = 100;                    /* minimum width when centered */
 static int fuzzy = 1;                      /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
