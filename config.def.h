b921d264e8dd511306d85d6f97a973de8bba874f
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-03-17 01:14:02 -0300

    Diminuído impacto do tamanho da fonte na altura

--- config.def.h
+++ config.def.h
@@ -34,8 +34,8 @@ static const unsigned int alphas[SchemeLast][2] = {
 static unsigned int lines      = 0;
 static unsigned int columns    = 0;
 /* -h option; minimum height of a menu line */
-static unsigned int lineheight = 20;
-static unsigned int min_lineheight = 20;
+static unsigned int lineheight = 18;
+static unsigned int min_lineheight = 8;
 
 
 /*
