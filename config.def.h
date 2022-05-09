f663524369659500fea014a6b3f252c504a29879
LucasTavaresA <tavares.lassuncao@gmail.com>
2022-05-09 00:12:34 -0300

    Usando a fonte terminus

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const unsigned int alpha = 0xe6;
 static int fuzzy = 1;              /* -F  option; if 0, dmenu doesn't use fuzzy matching     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-    "Fira Code:pixelsize=16:antialias=true:autohint=true",
+    "Terminus:pixelsize=18:antialias=true:autohint=true",
     "SauceCodePro Nerd Font Mono:weight=bold:size=20:antialias=true:hinting=true",
     "JoyPixels:pixelsize=16:antialias=true:autohint=true"
 };
